c9a66676b15f25adbe645b02bd80b4e80098636b
haramiee <96477522+haramiee@users.noreply.github.com>
2022-05-20 01:09:42 +0900

    02. 구구단 - 메소드 활용

--- Lectures/만들어 가면서 배우는 JAVA 플레이그라운드/Gugudan.java
+++ Lectures/만들어 가면서 배우는 JAVA 플레이그라운드/Gugudan.java
@@ -1,12 +1,22 @@
 public class Gugudan {
-	public static void main(String[] args) {
+	public static int[] calculate(int times) {
 		int[] result = new int[9];
-		for (int j=2; j < 10; j++) {
-			System.out.println(j + " 단!!");
-			for (int i=0; i < result.length; i++) {
-				result[i] = j * (i + 1);
-				System.out.println(result[i]);
-			}
+		for (int i=0; i < result.length; i++) {
+			result[i] = times * (i + 1);
+		}
+		return result;
+	}
+	
+	public static void print(int[] result) {
+		for (int i=0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		for (int i=2; i < 10; i++) {
+			int[] result = calculate(i);
+			print(result);
 		}
 	}
 }
