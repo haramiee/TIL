4ab0722c2250b88c75dcd029b35484659825ba40
haramiee <96477522+haramiee@users.noreply.github.com>
2022-05-20 01:11:32 +0900

    02. 구구단 - 최종 미션 ②
    사용자가 "8,7"과 같은 문자열을 입력하면 팔칠단을 구현한다. 팔칠단은 2 * 1 ... 2 * 7, 3 * 1 ... 3 * 7, ... , 8 * 1 ... 8 * 7 까지 구현하는 것을 의미한다.

--- Lectures/만들어 가면서 배우는 JAVA 플레이그라운드/Gugudan.java
+++ Lectures/만들어 가면서 배우는 JAVA 플레이그라운드/Gugudan.java
@@ -3,10 +3,15 @@ import java.util.Scanner;
 public class Gugudan {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int times = sc.nextInt();
-		for (int i=1; i <= 9; i++) {
-			int result = times * i;
-			System.out.println(times + " * " + i + " = " + result);
+		String str = sc.nextLine();
+		String[] splited = str.split(",");
+		int num1 = Integer.parseInt(splited[0]);
+		int num2 = Integer.parseInt(splited[1]);
+		for (int i=2; i <= num1; i++) {
+			System.out.println("######" + i + " 단!!");
+			for (int j=1; j <= num2; j++) {
+				System.out.println(i + " * " + j + " = " + i * j);
+			}
 		}
 	}
 }
