1e12dba87942bd94996ec1b0cf690b325715033d
haramiee <96477522+haramiee@users.noreply.github.com>
2022-05-20 01:08:39 +0900

    02. 구구단 - 반복문

--- Lectures/만들어 가면서 배우는 JAVA 플레이그라운드/Gugudan.java
+++ Lectures/만들어 가면서 배우는 JAVA 플레이그라운드/Gugudan.java
@@ -1,40 +1,15 @@
-import java.util.Scanner;
-
 public class Gugudan {
 	public static void main(String[] args) {
-		/* 4단 - 변수 할당 */
-		System.out.println("4단");
-		int result = 4 * 1;
-		System.out.println(result);
-		result = 4 * 2;
-		System.out.println(result);
-		result = 4 * 3;
-		System.out.println(result);
-		result = 4 * 4;
-		System.out.println(result);
-		result = 4 * 5;
-		System.out.println(result);
-		result = 4 * 6;
-		System.out.println(result);
-		result = 4 * 7;
-		System.out.println(result);
-		result = 4 * 8;
-		System.out.println(result);
-		result = 4 * 9;
-		System.out.println(result);
+		/* 6단 */
+		int i = 1;
+		while (i < 10) {
+			System.out.println(6 * i);
+			i++;
+		}
 		
-		/* 사용자 입력 값 받기 */
-		System.out.println("구구단 중 출력할 단은? ");
-		Scanner sc = new Scanner(System.in);
-		int number = sc.nextInt();
-		System.out.println(number * 1);
-		System.out.println(number * 2);
-		System.out.println(number * 3);
-		System.out.println(number * 4);
-		System.out.println(number * 5);
-		System.out.println(number * 6);
-		System.out.println(number * 7);
-		System.out.println(number * 8);
-		System.out.println(number * 9);
+		/* 7단 */
+		for (int j=1; j<10; j++) {
+			System.out.println(7 * j);
+		}
 	}
 }
