1209a7fb1b43bc9a655d0f2a890c7b3fae2456c1
haramiee <96477522+haramiee@users.noreply.github.com>
2022-05-20 01:11:04 +0900

    02. 구구단 - 최종 미션 ①
    사용자가 입력한 값에 따라 크기가 다른 구구단을 계산해 출력하기

--- Lectures/만들어 가면서 배우는 JAVA 플레이그라운드/Gugudan.java
+++ Lectures/만들어 가면서 배우는 JAVA 플레이그라운드/Gugudan.java
@@ -1,15 +1,12 @@
+import java.util.Scanner;
+
 public class Gugudan {
-	public static int[] calculate(int times) {
-		int[] result = new int[9];
-		for (int i=0; i < result.length; i++) {
-			result[i] = times * (i + 1);
-		}
-		return result;
-	}
-	
-	public static void print(int[] result) {
-		for (int i=0; i < result.length; i++) {
-			System.out.println(result[i]);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int times = sc.nextInt();
+		for (int i=1; i <= 9; i++) {
+			int result = times * i;
+			System.out.println(times + " * " + i + " = " + result);
 		}
 	}
 }
