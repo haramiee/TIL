f260e2ccb062c018958139da03c4bc5e6a786526
haramiee <96477522+haramiee@users.noreply.github.com>
2022-05-20 01:01:59 +0900

    02. 구구단 - 반복문

---
+++ Lectures/만들어 가면서 배우는 JAVA 플레이그라운드/Gugudan03.java
@@ -0,0 +1,15 @@
+public class Gugudan {
+	public static void main(String[] args) {
+		/* 6단 */
+		int i = 1;
+		while (i < 10) {
+			System.out.println(6 * i);
+			i++;
+		}
+		
+		/* 7단 */
+		for (int j=1; j<10; j++) {
+			System.out.println(7 * j);
+		}
+	}
+}
