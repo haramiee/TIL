41db1d9a63852bfe8686ba042c27394b39b1b2f5
haramiee <96477522+haramiee@users.noreply.github.com>
2022-05-20 01:08:56 +0900

    02. 구구단 - 값 입력 및 조건문

--- Lectures/만들어 가면서 배우는 JAVA 플레이그라운드/Gugudan.java
+++ Lectures/만들어 가면서 배우는 JAVA 플레이그라운드/Gugudan.java
@@ -1,15 +1,18 @@
+import java.util.Scanner;
+
 public class Gugudan {
 	public static void main(String[] args) {
-		/* 6단 */
-		int i = 1;
-		while (i < 10) {
-			System.out.println(6 * i);
-			i++;
-		}
-		
-		/* 7단 */
-		for (int j=1; j<10; j++) {
-			System.out.println(7 * j);
+		System.out.println("구구단 중 출력할 단은? ");
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		if (number < 2) {
+			System.out.println("값을 잘못 입력하셨습니다.");
+		} else if (number > 9) {
+			System.out.println("값을 잘못 입력하셨습니다.");
+		} else {
+			for (int i=1; i<10; i++) {
+				System.out.println(number * i);
+			}
 		}
 	}
 }
