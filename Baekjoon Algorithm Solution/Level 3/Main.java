fdf0e921d80ed6d1aea43aa54f06e7e00ba4868d
haramiee <96477522+haramiee@users.noreply.github.com>
2022-06-02 18:50:55 +0900

    [220602] 11021번 : A+B - 7

--- Baekjoon Algorithm Solution/Level 3/Main.java
+++ Baekjoon Algorithm Solution/Level 3/Main.java
@@ -3,14 +3,17 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main{
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int N = Integer.parseInt(br.readLine());
-		for (int i=N; i>0; i--) {
-			bw.write(i + "\n");
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		for (int i=1; i<=T; i++) {
+			st = new StringTokenizer(br.readLine());
+			bw.write("Case #" + i + ": " + (Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
 		}
 		bw.close();
 	}
