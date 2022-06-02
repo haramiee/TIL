5fe5736fc134133a7cf03558014abe36baae2cbc
haramiee <96477522+haramiee@users.noreply.github.com>
2022-06-02 18:49:59 +0900

    [220531] 15552번 : 빠른 A+B

--- Baekjoon Algorithm Solution/Level 3/Main.java
+++ Baekjoon Algorithm Solution/Level 3/Main.java
@@ -1,17 +1,20 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main{
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int T = Integer.parseInt(br.readLine());
 		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		for (int i=1; i <= T; i++) {
+		for (int i=1; i<=T; i++) {
 			st = new StringTokenizer(br.readLine());
-			sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
-			sb.append("\n");
+			bw.write( (Integer.parseInt(st.nextToken()) + (Integer.parseInt(st.nextToken()))) + "\n" );
 		}
-		System.out.println(sb);
+		bw.close();
     }
 }
