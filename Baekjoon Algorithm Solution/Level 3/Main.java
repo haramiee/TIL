ad396ab878f2cdc1bf2e00139317d47c7b3b9c83
haramiee <96477522+haramiee@users.noreply.github.com>
2022-06-02 18:50:38 +0900

    [220602] 2742번 : 기찍 N

--- Baekjoon Algorithm Solution/Level 3/Main.java
+++ Baekjoon Algorithm Solution/Level 3/Main.java
@@ -1,17 +1,17 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.IOException;
 
 public class Main{
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
-		for (int i=1; i<=N; i++) {
+		for (int i=N; i>0; i--) {
 			bw.write(i + "\n");
 		}
 		bw.close();
-    }
+	}
 }
