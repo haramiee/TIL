f1fe269c87c618ac208e129487712c8dddc7d3a3
haramiee <96477522+haramiee@users.noreply.github.com>
2022-06-02 18:48:05 +0900

    [220519] 2884번 : 알람 시계

--- Baekjoon Algorithm Solution/Level 2/Main.java
+++ Baekjoon Algorithm Solution/Level 2/Main.java
@@ -1,19 +1,27 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int A = Integer.parseInt(br.readLine());
-        int B = Integer.parseInt(br.readLine());
-        	if (A > 0 && B > 0)
-			    System.out.println("1");
-		    if (A < 0 && B > 0)
-			    System.out.println("2");
-		    if (A < 0 && B < 0)
-			    System.out.println("3");
-		    if (A > 0 && B < 0)
-			    System.out.println("4");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int hour, min;
+        if (B >= 45) {
+            min = B - 45;
+            System.out.println(A + " " + min);
+        } else if (B < 45) {
+            min = 60 - (45 - B);
+            if (A != 0) {
+                hour = A - 1;
+                System.out.println(hour + " " + min);
+            } else if (A == 0) {
+                int HOUR = 23;
+                System.out.println(HOUR + " " + min);
+            }
+        }
     }
 }
