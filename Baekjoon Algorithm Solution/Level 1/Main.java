e765fb44c974095f422f2d5ac6574e06717474cb
haramiee <96477522+haramiee@users.noreply.github.com>
2022-06-02 18:43:28 +0900

    10869번 : 사칙연산

--- Baekjoon Algorithm Solution/Level 1/Main.java
+++ Baekjoon Algorithm Solution/Level 1/Main.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
-        System.out.println("\\    /\\");
-        System.out.println(" )  ( ')");
-        System.out.println("(  /  )");
-        System.out.println(" \\(__)|");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
     }
 }
