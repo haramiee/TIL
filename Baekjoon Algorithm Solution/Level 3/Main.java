ea40f00f32b7f8bd92dbc72225e1cb553bd74e53
haramiee <96477522+haramiee@users.noreply.github.com>
2022-06-02 18:51:26 +0900

    [220602] 2438번 : 별 찍기 - 1

--- Baekjoon Algorithm Solution/Level 3/Main.java
+++ Baekjoon Algorithm Solution/Level 3/Main.java
@@ -3,19 +3,17 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class Main{
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int T = Integer.parseInt(br.readLine());
-		StringTokenizer st;
-		for (int i=1; i<=T; i++) {
-			st = new StringTokenizer(br.readLine());
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-			bw.write("Case #" + i + ": " + A + " + " + B + " = " + (A+B) + "\n");
+		int N = Integer.parseInt(br.readLine());
+		for (int i=1; i<=N; i++) {
+			for (int j=1; j<=i; j++) {
+				bw.write("*");
+			}
+			bw.write("\n");
 		}
 		bw.close();
 	}
