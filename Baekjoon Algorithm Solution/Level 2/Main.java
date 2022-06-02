79c58d01e2721c687760ea0ae0413740848d90ef
haramiee <96477522+haramiee@users.noreply.github.com>
2022-06-02 18:46:41 +0900

    9498번 : 시험 성적

--- Baekjoon Algorithm Solution/Level 2/Main.java
+++ Baekjoon Algorithm Solution/Level 2/Main.java
@@ -1,21 +1,22 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
-public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-		
-		if (A > B)
-			System.out.println(">");
-		else if (A < B)
-			System.out.println("<");
-		else if (A == B)
-			System.out.println("==");
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        if (A >= 90)
+            System.out.println("A");
+        else if (A >= 80)
+            System.out.println("B");
+        else if (A >= 70)
+            System.out.println("C");
+        else if (A >= 60)
+            System.out.println("D");
+        else
+            System.out.println("F");
+    }
 }
