811f4d05852c385655c6a7dab905ce4d96a29475
haramiee <96477522+haramiee@users.noreply.github.com>
2022-06-03 17:13:52 +0900

    [220603] 10871번 : X보다 작은 수

--- Baekjoon Algorithm Solution/Level 3/Main.java
+++ Baekjoon Algorithm Solution/Level 3/Main.java
@@ -1,19 +1,22 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        for (int i=1; i<=N; i++) {
-            for (int j=1; j<=N-i; j++) {
-                System.out.print(" ");
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<N; i++) {
+            int val = Integer.parseInt(st.nextToken());
+            if (val < X) {
+             sb.append(val).append(" ");   
             }
-            for (int k=1; k<=i; k++) {
-                System.out.print("*");
-            }
-            System.out.print("\n");
         }
-    }
+        System.out.print(sb);
+     }
 }
