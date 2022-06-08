254c4d4008477464d3dd85ee74a65b61c96e3c4b
haramiee <96477522+haramiee@users.noreply.github.com>
2022-06-08 22:04:55 +0900

    [220608] 1110번 : 더하기 사이클

--- Baekjoon Algorithm Solution/Level 3/Main.java
+++ Baekjoon Algorithm Solution/Level 3/Main.java
@@ -1,20 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-public class Main{
-	public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        String in;
-        while ((in = br.readLine()) != null) {
-            st = new StringTokenizer(in, " ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            sb.append(a+b).append("\n");
-        }
-        System.out.println(sb);
-    }
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		/* 주어진 수 N, 새로운 수 T */
+		int N = Integer.parseInt(br.readLine()); 	/* 처음 입력 값 */
+		int copy = N; 								/* 입력 값 복사 */
+		int count = 0;								/* 몇 번 반복됐는지 카운트 */
+		
+		while (true) {
+			N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+			count++;
+			
+			if (copy == N) {
+				break;
+			}
+		}
+		
+		System.out.println(count);
+	}
 }
