508ab746fe68d0e02264c88d3e2c72ec045d5fe8
haramiee <96477522+haramiee@users.noreply.github.com>
2022-06-11 23:58:19 +0900

    [220611] 2577번 : 숫자의 개수

--- Baekjoon Algorithm Solution/Level 4/Main.java
+++ Baekjoon Algorithm Solution/Level 4/Main.java
@@ -1,30 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int[] ar = new int[9];
-		for (int i = 0; i < 9; i++) {
-			ar[i] = Integer.parseInt(br.readLine());
+		int[] arr = new int[10];
+		
+		int sum = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
+		
+		String str = String.valueOf(sum);			/* 합을 문자열로 바꿈 */
+		
+		for (int i=0; i<str.length(); i++) {		/* 예를 들어 sum이 1703700이면 7번 돌면서 */
+			arr[(str.charAt(i) - 48)]++;			/* 1번 : arr[1]++; -> 1번 자리 값 증가 */
+													/* 2번 : arr[7]++; -> 7번 자리 값 증가 */
 		}
 		
-		int max = 0;
-		int idx = 0;
-		int cnt = 0;
-		
-		for (int val : ar) {
-			cnt++;
-			if (val > max) {
-				max = value;
-				idx = cnt;
-			}
+		for (int val : arr) {
+			System.out.println(val);				/* 하나씩 프린트 */
 		}
 		
-		System.out.println(max);
-		System.out.println(idx);
 	}
 }
