33b78147c60bf30f18d8f4d89ae4fc32f3dc9ce7
haramiee <96477522+haramiee@users.noreply.github.com>
2022-06-12 19:34:22 +0900

    [220612] 3052번 : 나머지

--- Baekjoon Algorithm Solution/Level 4/Main.java
+++ Baekjoon Algorithm Solution/Level 4/Main.java
@@ -5,20 +5,22 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int[] arr = new int[10];
 		
-		int sum = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
+		/* 정수 % 42의 최댓값은 41이므로, 42 길이 boolean 배열을 생성한다. */
+		boolean[] arr = new boolean[42];
 		
-		String str = String.valueOf(sum);			/* 합을 문자열로 바꿈 */
-		
-		for (int i=0; i<str.length(); i++) {		/* 예를 들어 sum이 1703700이면 7번 돌면서 */
-			arr[(str.charAt(i) - 48)]++;			/* 1번 : arr[1]++; -> 1번 자리 값 증가 */
-													/* 2번 : arr[7]++; -> 7번 자리 값 증가 */
+		/* 정수 % 42를 인덱스로, 그 값을 true로 바꿔 준다. */
+		/* 예를 들어 나머지가 1이면 arr[1] 값이 true로 바뀐다. -> true가 몇 개인지 세어 주면 된다. */
+		for (int i=0; i<10; i++) {
+			arr[Integer.parseInt(br.readLine()) % 42] = true;
 		}
 		
-		for (int val : arr) {
-			System.out.println(val);				/* 하나씩 프린트 */
+		/* true 값을 담을 cnt를 생성하고 값이 true일 때마다 증가시킨다. */
+		int cnt = 0;
+		for (boolean val : arr) {
+			if (val == true) cnt++;
 		}
 		
+		System.out.println(cnt);
 	}
 }
