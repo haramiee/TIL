e767422b03c3ab3775e25bee060341ec75b51d7d
haramiee <96477522+haramiee@users.noreply.github.com>
2022-06-12 19:55:01 +0900

    [220612] 1546번 : 평균

--- Baekjoon Algorithm Solution/Level 4/Main.java
+++ Baekjoon Algorithm Solution/Level 4/Main.java
@@ -1,26 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		/* 정수 % 42의 최댓값은 41이므로, 42 길이 boolean 배열을 생성한다. */
-		boolean[] arr = new boolean[42];
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		/* 정수 % 42를 인덱스로, 그 값을 true로 바꿔 준다. */
-		/* 예를 들어 나머지가 1이면 arr[1] 값이 true로 바뀐다. -> true가 몇 개인지 세어 주면 된다. */
-		for (int i=0; i<10; i++) {
-			arr[Integer.parseInt(br.readLine()) % 42] = true;
+		for (int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		/* true 값을 담을 cnt를 생성하고 값이 true일 때마다 증가시킨다. */
-		int cnt = 0;
-		for (boolean val : arr) {
-			if (val == true) cnt++;
+		int max = 0;
+		for (int val : arr) {
+			if (val > max) {
+				max = val;
+			}
 		}
 		
-		System.out.println(cnt);
+		double[] ar = new double[N];
+		
+		double sum = 0;
+		for (int i=0; i<N; i++) {
+			ar[i] = (double)arr[i] / (double)max * 100;
+			sum += ar[i];
+		}
+		
+		System.out.println((double) sum / N);
 	}
 }
